5eca39ad3cbe412be2c8b3a5f01d6461ff8a2686
Cabriya29 <cabriyaf@gmail.com>
2024-05-27 11:11:27 +0530

    updated method

--- programming.java
+++ programming.java
@@ -1,9 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class programming {
     public static void main(string)
 string name;
     int age;
     string email;
     int number;
-    
+    LSOutput
 
 }
